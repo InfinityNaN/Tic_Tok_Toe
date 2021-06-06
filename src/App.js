606e7726745b1fc4eda8ae02b19809b8971260b9
Abhishek Choudhary <abhishekchoudhary@Abhisheks-MacBook-Pro.local>
2021-06-06 23:20:58 +0530

    basic lockdown

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Icon from "./Components/Icon"
-import "./style.css"
 // import react toastify
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 // import react strap
 import 'bootstrap/dist/css/bootstrap.css';
 import { Button, Row, Col, Card, CardBody, Container } from 'reactstrap';
+import "./style.css"
 
+ const ticArray = new Array(9).fill("");
+ const App = ()=>{
+    let [isCross, setIsCross] = useState(false)
+    let [winMessage, setWinMessage] = useState("")
+    let [select, setSelect] = useState("x")
 
+    const reloadGame=()=>{
 
+    }
+    const checkIsWinner=()=>{
 
+    }
+    const changeItem=()=>{
 
-const App = ()=>{
+    }
     return(
-    <Icon  choice="circle"/>
+        <Container>
+            <Row>
+                <Col md={6} className="offset-md-3">
+                    
+                </Col>
+            </Row>
+        </Container>
     )
-}
 
+
+
+}
 export default App;
